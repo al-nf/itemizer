a5f41226ec676acde6fb1c1a71ab490829696161
afung <afung@zeus.local>
2024-11-17 23:42:05 -0800

    yep

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
-use actix_web::{web, App, HttpServer, Responder};
+use actix_web::{get, web, App, HttpServer};
 
-async fn index() -> impl Responder {
-    "Hello world!"
+struct AppState {
+    app_name: String, //structure containing a String app_name
+}
+
+#[get("/")]
+async fn index(data: web::Data<AppState>) -> String { // asynchronous function index
+    let app_name = &data.app_name; // pulls in app name, borrows from argument
+    format!("Hello {app_name}!") // returns it in reformatted
 }
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new().service(
-            // prefixes all resources and routes attached to it...
-            web::scope("/app")
-                // ...so this handles requests for `GET /app/index.html`
-                .route("/index.html", web::get().to(index)),
-        )
+async fn main() -> std::io::Result<()> { //main function which returns a Result enum
+    HttpServer::new(|| { //startingup a httpserver
+        App::new() //new application, where:
+            .app_data(web::Data::new(AppState { //set app data to a new AppState
+                app_name: String::from("Actix Web"), // where the item appname is set to "Actix
+                                                     // Web"
+            }))
+            .service(index) // call index, which returns the app reformatted.
     })
     .bind(("127.0.0.1", 8080))?
     .run()
