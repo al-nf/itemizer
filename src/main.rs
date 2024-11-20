b8165b410ed748573a7e365d09b6ef17a1815f82
afung <afung@zeus.local>
2024-11-20 00:21:18 -0800

    yep

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
 // src/main.rs
 
 use actix_web::{web, App, HttpServer, Responder, HttpResponse};
-mod downloadlink;
+use tokio;
+mod download;
 
 async fn get_link() -> impl Responder {
-    match downloadlink::get_latest_patch().await {
+    match download::get_latest_patch().await {
         Ok(version) => HttpResponse::Ok().json(version),
         Err(_) => HttpResponse::InternalServerError().body("failed to fetch latest version"),
     }
 }
 
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    /*
+    tokio::spawn(async {
+    if let Err(e) = download::download_and_extract().await {
+        eprintln!("Error during download or extraction: {}", e);
+    }
+    });
+    */
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(get_link)
