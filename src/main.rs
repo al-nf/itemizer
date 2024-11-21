8c04662ba74b4b9ea9a911bc3d0f3f634bbb1e43
afung <afung@zeus.local>
2024-11-20 22:51:24 -0800

    uh

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,10 @@ mod champion;
 mod item;
 mod runes;
 
+async fn construction() -> impl Responder {
+    HttpResponse::Ok().body("||| SITE UNDER CONSTRUCTION |||")
+}
+
 async fn download_file() -> impl Responder {
     match download::download_and_extract().await {
         Ok(_) => HttpResponse::Ok().body("File downloaded and extracted successfully."),
@@ -22,10 +26,11 @@ async fn download_file() -> impl Responder {
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(download_file)) 
+            .route("/download", web::get().to(download_file)) 
             .route("/champion", web::get().to(champion::get_champion_json))
             .route("/item", web::get().to(item::get_item_json))
             .route("/runes", web::get().to(runes::get_runes_json))
+            .route("/", web::get().to(construction))
     })
     .bind("127.0.0.1:8080")?  
     .run()
